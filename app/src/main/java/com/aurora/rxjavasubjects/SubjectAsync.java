35a1204cf6b4228272a7e4883479ae313c7f4b5d
monem validi <a.validi6016@gmail.com>
2021-02-20 17:41:33 +0330

    add SubjectAsynce

---
+++ app/src/main/java/com/aurora/rxjavasubjects/SubjectAsync.java
@@ -0,0 +1,146 @@
+package com.aurora.rxjavasubjects;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import io.reactivex.rxjava3.subjects.AsyncSubject;
+
+public class SubjectAsync extends AppCompatActivity {
+
+    private final static String TAG = "myApp";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject_async);
+
+        asyncSubjectDemo1();
+    }
+
+
+    void asyncSubjectDemo1() {
+
+        Observable<String> observable = Observable.just("JAVA", "KOTLIN", "XML", "JSON")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+
+        AsyncSubject<String> asyncSubject = AsyncSubject.create();
+
+        observable.subscribe(asyncSubject);
+
+        asyncSubject.subscribe(getFirstObserver());
+        asyncSubject.subscribe(getSecondObserver());
+        asyncSubject.subscribe(getThirdObserver());
+
+
+    }
+
+    private Observer<String> getFirstObserver() {
+
+        Observer<String> observer = new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+
+                Log.i(TAG, " First Observer onSubscribe ");
+            }
+
+            @Override
+            public void onNext(String s) {
+
+                Log.i(TAG, " First Observer Received " + s);
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+                Log.i(TAG, " First Observer onError ");
+            }
+
+            @Override
+            public void onComplete() {
+
+                Log.i(TAG, " First Observer onComplete ");
+
+            }
+        };
+
+        return observer;
+    }
+
+    private Observer<String> getSecondObserver() {
+
+        Observer<String> observer = new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+
+                Log.i(TAG, " Second Observer onSubscribe ");
+            }
+
+            @Override
+            public void onNext(String s) {
+
+                Log.i(TAG, " Second Observer Received " + s);
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+                Log.i(TAG, " Second Observer onError ");
+            }
+
+            @Override
+            public void onComplete() {
+
+                Log.i(TAG, " Second Observer onComplete ");
+
+            }
+        };
+
+        return observer;
+    }
+
+    private Observer<String> getThirdObserver() {
+
+        Observer<String> observer = new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+
+                Log.i(TAG, " Third Observer onSubscribe ");
+            }
+
+            @Override
+            public void onNext(String s) {
+
+                Log.i(TAG, " Third Observer Received " + s);
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+                Log.i(TAG, " Third Observer onError ");
+            }
+
+            @Override
+            public void onComplete() {
+
+                Log.i(TAG, " Third Observer onComplete ");
+
+            }
+        };
+
+        return observer;
+    }
+
+}
